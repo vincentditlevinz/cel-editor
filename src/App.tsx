b1e17d21cf31a92b38f03ddc20b1c6bb0229f4e5
Vincent MATHON <vincentditlevinz@users.noreply.github.com>
2022-12-19 16:45:35 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,50 @@
+import { teal } from '@mui/material/colors';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { QueryBuilderMaterial } from '@react-querybuilder/material';
+import { useState } from 'react';
+import type { Field, RuleGroupType } from 'react-querybuilder';
+import { formatQuery, QueryBuilder } from 'react-querybuilder';
+import './styles.scss';
+
+const muiTheme = createTheme({
+  palette: {
+    secondary: {
+      main: teal[500],
+    },
+  },
+});
+
+const fields: Field[] = [
+  { name: 'firstName', label: 'First Name' },
+  { name: 'lastName', label: 'Last Name' },
+];
+
+const initialQuery: RuleGroupType = {
+  combinator: 'and',
+  rules: [
+    { field: 'firstName', operator: 'beginsWith', value: 'Stev' },
+    { field: 'lastName', operator: 'in', value: 'Vai,Vaughan' },
+  ],
+};
+
+export const App = () => {
+  const [query, setQuery] = useState(initialQuery);
+
+  return (
+    <div>
+      <ThemeProvider theme={muiTheme}>
+        <QueryBuilderMaterial>
+          <QueryBuilder
+            fields={fields}
+            query={query}
+            onQueryChange={q => setQuery(q)}
+          />
+        </QueryBuilderMaterial>
+      </ThemeProvider>
+      <h4>Query</h4>
+      <pre>
+        <code>{formatQuery(query, 'json')}</code>
+      </pre>
+    </div>
+  );
+};
